a7dbd9caa20812d669382bc7a062bf8decb67cd6
Goutte <antoine.goutenoir@gmail.com>
2012-11-17 13:17:06 +0100

    Cleanup TablePrefixSubscriber

--- Subscriber/TablePrefixSubscriber.php
+++ Subscriber/TablePrefixSubscriber.php
@@ -36,16 +36,6 @@ class TablePrefixSubscriber implements EventSubscriber
         $prefix = $this->getTablePrefix($args);
 
         $classMetadata->setPrimaryTable(array('name'=>$prefix.$classMetadata->getTableName()));
-        //$classMetadata->setTableName($prefix.$classMetadata->getTableName());
-
-        // TODO: Looks like we don't need these lines for many to many anymore, it might result in duplicated table prefix.
-
-        // foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
-        //     if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY) {
-        //         $mappedTableName = $classMetadata->associationMappings[$fieldName]['joinTable']['name'];
-        //         $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $prefix . $mappedTableName;
-        //     }
-        // }
     }
 
     private function getTablePrefix(LoadClassMetadataEventArgs $args)
