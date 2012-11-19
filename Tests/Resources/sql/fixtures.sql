934bc8fb2dfe82c9709392e4437bb76b22278c1d
Goutte <antoine.goutenoir@gmail.com>
2012-11-19 05:23:45 +0100

    Adding minimalistic test fixtures

---
+++ Tests/Resources/sql/fixtures.sql
@@ -0,0 +1,5 @@
+
+-- POSTS
+
+INSERT INTO `wp_posts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`) VALUES
+(3, 1, '2012-11-19 01:38:22', '2012-11-19 01:38:22', 'Welcome to WordPress. This is your first post draft. Edit or delete it, then start testing!', 'Hello world 2!', 'This is your first post draft.', 'draft', 'open', 'open', '', 'hello-world-2', '', '', '2012-11-19 01:38:22', '2012-11-19 01:38:22', '', 0, 'http://localhost/FREE/shine/mob/wp/?p=3', 0, 'post', '', 1);
